944cba14226d27a64c113e88df87a5952f02d02f
Reksford <rexfordjwoods@yahoo.com>
2021-12-30 18:30:47 -0700

    Comments out debug button

--- src/components/liveFeed/ActiveFeed.js
+++ src/components/liveFeed/ActiveFeed.js
@@ -2,7 +2,7 @@ import {useFeed} from '../../providers/FeedProvider';
 
 const ActiveFeed = (props) => {
   const {sendFish, textFeed} = useFeed();
-  let tempFish = {name: 'flounder', size: 12.5, type: 'medium'}
+  // let tempFish = {name: 'flounder', size: 12.5, type: 'medium'}
 
   return (
     <div className="d-flex flex-column flex-grow-1" style={{padding: '.5rem 1rem'}}>
@@ -14,7 +14,7 @@ const ActiveFeed = (props) => {
           })}
         </div>
       </div>
-      <button onClick={() => sendFish(tempFish)}>New Fish</button>
+      {/* <button onClick={() => sendFish(tempFish)}>New Fish</button> */}
     </div>
   )
 }

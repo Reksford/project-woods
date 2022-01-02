416571e4b67cf4821a2f805bdeabbedd1bcd86d1
Reksford <rexfordjwoods@yahoo.com>
2022-01-01 22:40:13 -0700

    Fixes provider import

--- src/components/liveFeed/ActiveFeed.js
+++ src/components/liveFeed/ActiveFeed.js
@@ -1,7 +1,7 @@
 import {useFeed} from '../../providers/FeedProvider';
 
 const ActiveFeed = (props) => {
-  // const {sendFish, textFeed} = useFeed();
+  const { textFeed } = useFeed();
   // let tempFish = {name: 'flounder', size: 12.5, type: 'medium'}
   return (
     <div className="d-flex flex-column flex-grow-1" style={{padding: '.5rem 1rem'}}>
